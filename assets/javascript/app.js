af2e54093c1d8bad03fa97737111c2cf1bb6c746
Michael Dillie <michaeldillie@gmail.com>
2018-04-26 13:27:21 -0500

    Complete redo of project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,70 @@
 $(document).ready(function () {
-    var startPage = $(".game-start-page");
-    var questionOne = $("#q-1");
-    var questionTwo = $("#q-2");
+    var questionList = [{
+            "question": "What dose DOM stand for",
+            "answers": ["Digital Ohm Meter", "Do Octopus Meditate", "Document Object Model", "Dirty Old Man"],
+            "correctAnswer": "Document Object Model"
+        },
+        {
+            "question": "Is 1 === '1'",
+            "answers": ["Yes", "No"],
+            "correctAnswer": "No"
+        },
+        {
+            "question": "Which of these is an anchor tag",
+            "answers": ["<p>", "src=''", "<div>", "<a>"],
+            "correctAnswer": "<a>"
+        },
+        {
+            "question": "What dose API stand for",
+            "answers": ["Application Programming Interface", "Academic Performance Index", "Alabama Polytechnic Institute", "American Paper Institute"],
+            "correctAnswer": "Application Programming Interface"
+        },
+        {
+            "question": "What dose color: change in CSS",
+            "answers": ["font-size", "background color", "height", "text color"],
+            "correctAnswer": "text color"
+        }
+    ];
 
-    var timeLeft = 10;
-    var intervalId;
+    // console.log(questionList[0].answers[2]);
 
 
-    $("#start-game-btn").click(function() {
-        startPage.hide();
-        questionOne.show();
-        timer();
+    var currentQuestion = 0;
+    // Page Variables
+    var landingPage = $(".landing-page");
+    var questionsPage = $(".questions-page");
+    // Start BTN
+    var startGameBtn = $(".start-game-btn");
+    // Question and Answers Variables
+    var questionTitle = $(".question-title");
+    var answerOne = $(".answer-1");
+    var answerTwo = $(".answer-2");
+    var answerThree = $(".answer-3");
+    var answerFour = $(".answer-4");
+
+    var test = $(".answer");
+
+    // Satrts the game
+    function startGame() {
+        landingPage.hide();
+        questionsPage.show();
+        populate();
+    }
+    //  On click function
+    startGameBtn.on("click", function () {
+        startGame();
+    });
+    // Populate HTML funciton
+    function populate() {
+        questionTitle.append(questionList[currentQuestion].question);
+        answerOne.append(questionList[currentQuestion].answers[0]);
+        answerTwo.append(questionList[currentQuestion].answers[1]);
+        answerThree.append(questionList[currentQuestion].answers[2]);
+        answerFour.append(questionList[currentQuestion].answers[3]);
+    }
+
+    test.on("click", function () {
+        console.log(test.val());
     });
 
-    $(".answers").click(function() {
-        var test = $(this).attr("data-answervalue");
-        console.log(test);
-        if(test == 3) {
-            $("#correct").show();
-            $(".answers").hide();
-            $(".time-left").hide();
-            timeStop();
-        }
-        else {
-            $("#incorrect").show();
-            $(".answers").hide();
-            $(".time-left").hide();
-            timeStop();
-        }
-    });
-
-    function timer() {
-        intervalId = setTimeout(decrement, 1000);
-    }
-    function decrement() {
-        timeLeft--;
-        $("#time-left-num").text(timeLeft);
-        timer();
-        if(timeLeft === 0) {
-            $(".answers").hide();
-            $("#out-of-time").show();
-            $(".time-left").hide();
-            timeStop();
-        }
-    }
-    function timeStop() {
-        clearTimeout(intervalId);
-    }
 });
